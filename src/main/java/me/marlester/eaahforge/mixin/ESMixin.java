904a0cb266c9dea30c638d926c9cc5f3fb2c5a60
Marlester <77095872+marlester-dev@users.noreply.github.com>
2024-09-03 16:50:38 +0200

    1.20.6 update ESMIXIn

--- src/main/java/me/marlester/eaahforge/mixin/ESMixin.java
+++ src/main/java/me/marlester/eaahforge/mixin/ESMixin.java
@@ -3,16 +3,17 @@ package me.marlester.eaahforge.mixin;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(targets = {"net/minecraft/world/inventory/ArmorSlot"})
+@Mixin(targets = {"net/minecraft/world/inventory/InventoryMenu$1"})
 public class ESMixin {
-  EquipmentSlot slot;
+  EquipmentSlot val$equipmentslot;
   @Inject(method = "mayPlace(Lnet/minecraft/world/item/ItemStack;)Z", at = @At("RETURN"), cancellable = true)
   public void allowItemEquipping(ItemStack itemStack, CallbackInfoReturnable<Boolean> cir) {
-    if (slot == EquipmentSlot.HEAD) {
+    if (val$equipmentslot == EquipmentSlot.HEAD) {
       cir.setReturnValue(true);
     }
   }
