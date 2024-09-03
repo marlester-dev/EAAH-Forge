72ed78b928a73764f818274d26a20a44c6a5299c
Marlester <77095872+marlester-dev@users.noreply.github.com>
2024-09-03 15:31:33 +0200

    Delete build/moddev directory

--- build/moddev/runData.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-for /f "tokens=2 delims=:." %%x in ('chcp') do set _codepage=%%x
-chcp 65001>nul
-cd C:\Users\Ich\IdeaProjects\EAAHf\run
-C:\Users\Ich\.gradle\jdks\eclipse_adoptium-21-amd64-windows\jdk-21.0.4+7\bin\java.exe @C:\Users\Ich\IdeaProjects\EAAHf\build\moddev\dataRunClasspath.txt @C:\Users\Ich\IdeaProjects\EAAHf\build\moddev\dataRunVmArgs.txt -Dfml.modFolders=eaahforge%%%%C:\Users\Ich\IdeaProjects\EAAHf\build\classes\java\main;eaahforge%%%%C:\Users\Ich\IdeaProjects\EAAHf\build\resources\main net.neoforged.devlaunch.Main @C:\Users\Ich\IdeaProjects\EAAHf\build\moddev\dataRunProgramArgs.txt
-if not ERRORLEVEL 0 (  echo Minecraft failed with exit code %ERRORLEVEL%  pause)
-chcp %_codepage%>nul
-endlocal
